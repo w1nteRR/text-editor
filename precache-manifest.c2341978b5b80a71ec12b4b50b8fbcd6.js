1b2b37bf2292a1b0a8e7e9c40da439293f4b4a87
Vatamanyuk Mykola <mykola796@gmail.com>
2020-09-05 19:37:16 +0300

    Updates

---
+++ precache-manifest.c2341978b5b80a71ec12b4b50b8fbcd6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c52d979d65e625ebe158a2415078df52",
+    "url": "/text-editor/index.html"
+  },
+  {
+    "revision": "e68aa77d5bd194aedb85",
+    "url": "/text-editor/static/css/main.7c54ef30.chunk.css"
+  },
+  {
+    "revision": "69bfb2f19b6ad6d6276b",
+    "url": "/text-editor/static/js/2.0647c929.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/text-editor/static/js/2.0647c929.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e68aa77d5bd194aedb85",
+    "url": "/text-editor/static/js/main.b20238b4.chunk.js"
+  },
+  {
+    "revision": "a9180d7b928b86a79e4b",
+    "url": "/text-editor/static/js/runtime-main.24f782e7.js"
+  }
+]);
